529653a396844f966e0b6481017747134f8f6118
Vlad Spirin <vladspirin@hotmail.com>
2017-12-10 18:35:32 +0200

    Rename js files && add new info

---
+++ homework/Lesson2/js/task3.js
@@ -0,0 +1,9 @@
+"use strict";
+var str = prompt("Введите любой произвольный текст:", "");
+for (var i = 0; i <= s.length; i++) {
+    console.log(s[i]);
+    if (s[i] == "w") {
+        console.log("Буква" + s[i] + "на позиции " + i);
+        break;
+    }
+}
