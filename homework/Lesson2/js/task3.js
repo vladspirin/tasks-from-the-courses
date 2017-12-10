ec7f4e5eba9b2fffab09e4119ffe905063bece74
Vlad Spirin <vladspirin@hotmail.com>
2017-12-10 20:47:37 +0200

    Fixed mistake

--- homework/Lesson2/js/task3.js
+++ homework/Lesson2/js/task3.js
@@ -1,9 +1,2 @@
 "use strict";
-var str = prompt("Введите любой произвольный текст:", "");
-for (var i = 0; i <= s.length; i++) {
-    console.log(s[i]);
-    if (s[i] == "w") {
-        console.log("Буква" + s[i] + "на позиции " + i);
-        break;
-    }
-}
+var str = prompt("Enter some text:", "");
