0d4fb7c04caba5ecb6fee543bbf63f1400dd5c87
Vlad Spirin <vladspirin@hotmail.com>
2017-12-13 16:20:56 +0200

    Complete homework task 1

--- homework/Lesson3/js/task1.js
+++ homework/Lesson3/js/task1.js
@@ -1,1 +1,25 @@
 "use strict";
+var x, y, z;
+x = +prompt("Введите число ' x '", "");
+y = +prompt("Введите число ' y '", "");
+var j = +prompt("1 = ' + '; 2 = ' - ', 3 = ' * ', 4 = ' / '", "");
+switch (j) {
+  case 1:
+    z = x + y;
+    alert(z);
+    break;
+  case 2:
+    z = x - y;
+    alert(z);
+    break;
+  case 3:
+    z = x * y;
+    alert(z);
+    break;
+  case 4:
+    z = x / y;
+    alert(z);
+    break;
+  default:
+  alert("Операция невозможна.");
+}
