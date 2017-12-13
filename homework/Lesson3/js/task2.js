f99ad16b41b73742f875d55f593d9a757aeed408
Vlad Spirin <vladspirin@hotmail.com>
2017-12-13 17:26:17 +0200

    Save task 2 (not completed)

--- homework/Lesson3/js/task2.js
+++ homework/Lesson3/js/task2.js
@@ -1,1 +1,13 @@
+// Программа поиска необходимого оператора
 "use strict";
+var mobile = {
+  "Kyivstar": ["039", "067", "068", "096", "097", "098"],
+  "Vodafone": ["050", "066", "095", "099"],
+  "Lifecell": ["063", "093"],
+  "Intertelecom": ["094"],
+  "PEOPLEnet": ["092"],
+  "3Mob": ["091"]
+}
+for (var key in mobile) {
+  console.log("Вы в сети " + key);
+}
