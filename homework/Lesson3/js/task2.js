4de9297ef066bd4b6ffd895dc0a1e159cbacbbf8
Vlad Spirin <vladspirin@hotmail.com>
2017-12-13 18:52:08 +0200

    Update (del "") in keys

--- homework/Lesson3/js/task2.js
+++ homework/Lesson3/js/task2.js
@@ -1,13 +1,17 @@
 // Программа поиска необходимого оператора
 "use strict";
 var mobile = {
-  "Kyivstar": ["039", "067", "068", "096", "097", "098"],
-  "Vodafone": ["050", "066", "095", "099"],
-  "Lifecell": ["063", "093"],
-  "Intertelecom": ["094"],
-  "PEOPLEnet": ["092"],
-  "3Mob": ["091"]
+  Kyivstar: ["039", "067", "068", "096", "097", "098"],
+  Vodafone: ["050", "066", "095", "099"],
+  Lifecell: ["063", "093"],
+  Intertelecom: ["094"],
+  PEOPLEnet: ["092"],
+  TriMob: ["091"]
 }
+var user = prompt("Введите номер мобильного телефона в формате: 0001112233", "");
 for (var key in mobile) {
-  console.log("Вы в сети " + key);
+  console.log("Вас приветствует " + key);
 }
+console.log(mobile.Kyivstar);
+
+
