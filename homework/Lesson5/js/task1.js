5de0c7556c0a29c7bc1d6fb224749b1feacf4d35
Vlad Spirin <vladspirin@hotmail.com>
2017-12-21 11:23:10 +0200

    Not completed

--- homework/Lesson5/js/task1.js
+++ homework/Lesson5/js/task1.js
@@ -1,1 +1,18 @@
 "use strict";
+// Запрашиваем у пользователя необходимое число
+var x = +prompt("Введите число от 1 до 9: ", "");
+//Создаем объект с необходимыми данными
+var num = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
+// Создаем функцию которая будет переводит числа строковые названия
+function translate(xPar, numPar) {
+  for (var key in numPar) {
+    console.log(numPar[key]);
+    if (numPar[key] === xPar) {
+      alert("Вы ввели - " + key);
+    } else {
+      alert("Значение не принадлежит указанному диапазону");
+    }
+    break;
+  }
+};
+translate(x, num);
