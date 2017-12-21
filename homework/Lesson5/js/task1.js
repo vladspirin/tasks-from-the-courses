9eb87ee8f183af9e4ee98fb43d382ea7f09fcd7a
Vlad Spirin <vladspirin@hotmail.com>
2017-12-21 13:57:40 +0200

    Just comments

--- homework/Lesson5/js/task1.js
+++ homework/Lesson5/js/task1.js
@@ -6,10 +6,13 @@ var num = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight:
 // Создаем функцию которая будет переводит числа строковые названия
 function translate(xPar, numPar) {
   for (var key in numPar) {
+    // Перебираем в цикле объект
     console.log(numPar[key]);
     if (numPar[key] === xPar) {
+      // Выводим на экран результат совпадения
       alert("Вы ввели - " + key);
     } else {
+      // Выводим на экран результат несовпадения
       alert("Значение не принадлежит указанному диапазону");
     }
     break;
