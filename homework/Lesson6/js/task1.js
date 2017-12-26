45c0743f2726e100d0f96d4b0371c5ebdd7adbe5
Vlad Spirin <vladspirin@hotmail.com>
2017-12-26 15:34:48 +0200

    Added program structure

--- homework/Lesson6/js/task1.js
+++ homework/Lesson6/js/task1.js
@@ -1,1 +1,20 @@
 "use strict";
+// Создаем объект с инфо о сотруднике
+var collaborator = {
+  firstName: "Ivan",
+  lastName: "Ivanov",
+  patronym: "Ivanovich",
+  position: "director",
+  wageRate: 3000,
+  workExperience: 4
+};
+// Создаем функцию вычисления зароботной платы
+function calcWage() {
+  
+}
+calcWage();
+// Создаем функцию которая выводит информацию
+function outputInfo() {
+  
+}
+outputInfo();
