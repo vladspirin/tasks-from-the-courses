ebffded0b024d044873d977061cb4f3d712f6ab2
Vlad Spirin <vladspirin@hotmail.com>
2017-12-27 21:11:13 +0200

    Not finished..... I'm going to sleep

--- homework/Lesson7/Bootstrap template/js/script.js
+++ homework/Lesson7/Bootstrap template/js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+// Написать скрипт чтобы при изменении цвета заголовков второго уровня (h2), изменялся и цвет соответствующего ему текста (p).
+function getColorEl() {
+  var parElem = document.getElementsByClassName("col-md-4");
+  for (var i = 0; i < parElem.length; i++) {
+    parElem[i].style.color = "darkblue";
+  }
+}
+getColorEl();
+// Для кнопки Sign in найти ее ближайшую родительскую форму.
+function getParEl(params) {
+  var btn = document.getElementsByClassName("btn");
+  // btn.
+
+}
+getParEl();
