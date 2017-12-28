5acc576861d76417407dcbb766081c8b4eeb7128
Vlad Spirin <vladspirin@hotmail.com>
2017-12-28 18:36:22 +0200

    Added file structure

--- homework/Lesson7/Bootstrap template 2/js/task-script.js
+++ homework/Lesson7/Bootstrap template 2/js/task-script.js
@@ -1,1 +1,13 @@
 "use strict";
+// Удаляем заголовки
+function deleteHeadings() {
+
+}
+deleteHeadings();
+// Создаем массив объектов
+var arr = [{},{},{},{},{},{}];
+// Создаем новые зоголовки
+function createHeadins() {
+  
+}
+createHeadins();
