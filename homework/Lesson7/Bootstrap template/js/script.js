c6bdcc9fd7f5a106c8116f0863bc2572ca6aed8a
Vlad Spirin <vladspirin@hotmail.com>
2017-12-28 13:20:17 +0200

    Made on 75%

--- homework/Lesson7/Bootstrap template/js/script.js
+++ homework/Lesson7/Bootstrap template/js/script.js
@@ -1,16 +1,26 @@
 "use strict";
 // Написать скрипт чтобы при изменении цвета заголовков второго уровня (h2), изменялся и цвет соответствующего ему текста (p).
-function getColorEl() {
+function changeColorEl() {
   var parElem = document.getElementsByClassName("col-md-4");
   for (var i = 0; i < parElem.length; i++) {
     parElem[i].style.color = "darkblue";
   }
+  // Изменяем фон всех элементов с классом btn-default.
+  var clr = document.querySelectorAll(".btn-default");
+  for (var i = 0; i < clr.length; i++) {
+    clr[i].style.background = "#9472aa";
+  }
 }
-getColorEl();
+changeColorEl();
 // Для кнопки Sign in найти ее ближайшую родительскую форму.
-function getParEl(params) {
-  var btn = document.getElementsByClassName("btn");
-  // btn.
-
+function getParEl() {
+  var btn = document.querySelector(".btn-success");
+  var par = btn.closest(".navbar-form");
+  console.log(par);
 }
 getParEl();
+// Скрипт добавляющий строку меню с пунктами «Главная», «Деятельность» «Контакты».
+function addMenuItems() {
+  
+}
+addMenuItems();
