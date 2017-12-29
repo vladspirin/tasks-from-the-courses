b49618cf3826487a85993df71585a561d9d7a54e
Vlad Spirin <vladspirin@hotmail.com>
2017-12-29 14:31:12 +0200

    Added data attr, change styles, added bind func

--- homework/Lesson8/js/script-task1.js
+++ homework/Lesson8/js/script-task1.js
@@ -1,18 +1,33 @@
 "use strict";
-// Пишем функцию управления кнопкой на экране
+// Функция управления кнопкой на экране
 function clickOnButton() {
   var btn = document.getElementById("btn");
+  btn.addEventListener("click", function(event) {
+    event.target;
+  });
 
-    // console.log();
+  // console.log();
 }
 clickOnButton();
 
-// Пишем функцию управления модальным окном
+// Функция управления модальным окном
 function showModalWindow() {
-    var mod = document.getElementById("modWind");
-    var x = document.querySelector(".close");
+  var mod = document.getElementById("modWind");
+  var x = document.querySelector(".close");
 
-    // console.log();
-    // console.log();
+  // console.log();
+  // console.log();
 }
 showModalWindow();
+
+// Функция связывающая модальное окно с кнопкой
+function bindModalBtn() {
+  // Отслеживаем событие по клику
+  btn.addEventListener("click", function () {
+    var id = btn.dataset.tooltip; // привязываем блоки
+    var tltip = document.getElementById(id);
+    // Показываем скрытое окно
+    tltip.classList.toggle("show");
+  });
+}
+bindModalBtn();
