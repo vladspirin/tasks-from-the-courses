20fbf710f8a24363aa8edc5af371eb8928a92e5c
Vlad Spirin <vladspirin@hotmail.com>
2017-12-29 13:02:19 +0200

    JS file strucrure (I think what to do next)

--- homework/Lesson8/js/script-task1.js
+++ homework/Lesson8/js/script-task1.js
@@ -1,1 +1,18 @@
 "use strict";
+// Пишем функцию управления кнопкой на экране
+function clickOnButton() {
+  var btn = document.getElementById("btn");
+
+    // console.log();
+}
+clickOnButton();
+
+// Пишем функцию управления модальным окном
+function showModalWindow() {
+    var mod = document.getElementById("modWind");
+    var x = document.querySelector(".close");
+
+    // console.log();
+    // console.log();
+}
+showModalWindow();
