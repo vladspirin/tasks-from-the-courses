74d996d1bd64f123f1bc6b5a153f6544f9827e2b
Vlad Spirin <vladspirin@hotmail.com>
2017-12-29 16:38:13 +0200

    Shortened the code

--- homework/Lesson8/js/script-task1.js
+++ homework/Lesson8/js/script-task1.js
@@ -11,25 +11,20 @@ function btnClick() {
 btnClick();
 
 // Функция управления модальным окном
-function showModalWindow() {
+function ModalWindow() {
   var mod = document.getElementById("modWind");
   var x = document.querySelector(".close");
-  x.addEventListener("click", function() {
-  mod.classList.toggle("show"); 
-  });
-  // console.log();
-  // console.log();
-}
-showModalWindow();
-
-// Функция связывающая модальное окно с кнопкой
-function bindModalBtn() {
   // Отслеживаем событие по клику
   btn.addEventListener("click", function () {
-    var id = btn.dataset.tooltip; // привязываем блоки
+    // Связываем модальное окно с кнопкой
+    var id = btn.dataset.tooltip; 
     var tltip = document.getElementById(id);
     // Показываем скрытое окно
     tltip.classList.toggle("show");
   });
+  x.addEventListener("click", function() {
+  // Скрываем модальное окно  
+  mod.classList.toggle("show");   
+  });
 }
-bindModalBtn();
+ModalWindow();
