f0b91a06e8cbe0eb710988e8f0b540003ac77038
Vlad Spirin <vladspirin@hotmail.com>
2018-01-11 19:09:28 +0200

    Added Lesson 10 file struct

--- homework/Lesson9/js/script1.js
+++ homework/Lesson9/js/script1.js
@@ -1,1 +1,8 @@
 "use strict";
+var nav = document.getElementById('nav');
+nav.addEventListener('mouseover', function (e) {
+    var target = e.target;
+
+    var targetParent = target.closest('.item-tabs');
+    console.log(targetParent);
+});
