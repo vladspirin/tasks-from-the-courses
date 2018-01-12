b0d63fa7c98a36ef51df5ff7e73b952969c3e935
Vlad Spirin <vladspirin@hotmail.com>
2018-01-12 23:31:47 +0200

    Interim commit

--- homework/Lesson9/js/script1.js
+++ homework/Lesson9/js/script1.js
@@ -1,13 +1,19 @@
 "use strict";
 var tabs = document.getElementById('tabs');
 var activeTab = document.getElementsByClassName('tab')[0];
-
 tabs.addEventListener('click', function (e) {
     var target = e.target;
-    console.log(target);
     if (target.classList.contains('tab')) {
+        activeTab.classList.remove('activeStyles');
+        target.classList.add('activeStyles');
         document.getElementById(activeTab.dataset.tabBtn).classList.remove('active');
         document.getElementById(target.dataset.tabBtn).classList.add('active');
         activeTab = target;
     }
 });
+// var navTabs = document.getElementById('nav-tabs');
+// var passiveTab = document.getElementsByClassName('tab')[1];
+// navTabs.addEventListener('mousemove', function (e) {
+//     var target = e.target;
+
+// });
