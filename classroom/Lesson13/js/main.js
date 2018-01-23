77b3edcea7985ebed4caca64c4d2a696a280b39b
Vlad Spirin <vladspirin@hotmail.com>
2018-01-23 21:40:53 +0200

    Added Lesson 13

---
+++ classroom/Lesson13/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('#fullpage').fullpage({
+    sectionsColor: ['#ccc', '#88f', '#8f8', '#f88'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['firstSlide', 'secondSlide'],
+  });
+});
