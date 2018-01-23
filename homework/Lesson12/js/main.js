644efb4f0eb990e56037e9cd83effa3ac60d388a
Vlad Spirin <vladspirin@hotmail.com>
2018-01-23 21:42:46 +0200

    Added dropdown menu

--- homework/Lesson12/js/main.js
+++ homework/Lesson12/js/main.js
@@ -1,15 +1,23 @@
 $(document).ready(function () {
+  $('.collapsible').collapsible();
+
   $('.my-slider').slick({
     // adaptiveHeight,
     autoplay: true,
     autoplaySpeed: 6000,
     dots: true
   });
-  $('.fade').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
+
+  $(".dropdown-button").dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
   });
+
+
 });
