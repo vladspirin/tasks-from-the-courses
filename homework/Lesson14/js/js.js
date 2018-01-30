be97f088f1daf2463a1ccb458c3908aad84b4faa
Vlad Spirin <vladspirin@hotmail.com>
2018-01-30 17:54:21 +0200

    Added file struct, animate.css plugin, some styles

---
+++ homework/Lesson14/js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#yourElement').animateCss('bounce');
+  or;
+  $('#yourElement').animateCss('bounce', function () {
+    // Do somthing after animation
+  });
+});
+
