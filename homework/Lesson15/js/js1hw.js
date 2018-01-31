5152465a3fcf71402f9561e76ab290b4429e3ceb
Vlad Spirin <vladspirin@hotmail.com>
2018-02-01 00:57:57 +0200

    HW Lesson 15 (still open)

---
+++ homework/Lesson15/js/js1hw.js
@@ -0,0 +1,22 @@
+"use strict";
+var date = 12;
+var money = 100;
+var ob = {
+  3: 0.12 / 12,
+  6: 0.16 / 12,
+  12: 0.18 / 12
+};
+
+function calDep(datePar, moneyPar, obPar) {
+  for (var i in ob) {
+    if (i == datePar) {
+      var sum = moneyPar;
+      for (var j = 1; j <= datePar; j++) {
+        sum += sum * obPar[i];
+        console.log('month = ' + sum);
+      }
+      console.log('all = ' + sum);
+    }
+  }
+};
+calDep(date, money, ob);
